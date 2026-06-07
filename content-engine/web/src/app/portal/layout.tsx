31e5dcb53c3900e7ce50b6900d94fa7554cfdbd1
Sumit Sharma <sumitshrm12@gmail.com>
2026-06-07 18:29:59 +0530

    Delete content-engine directory

--- content-engine/web/src/app/portal/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PortalAuthProvider } from '@/lib/portalAuth';
-
-export default function PortalLayout({ children }: { children: React.ReactNode }) {
-  return <PortalAuthProvider>{children}</PortalAuthProvider>;
-}
